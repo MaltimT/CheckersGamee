bacf9023209c9ca964c3501a5eeaa31b411e0fdf
Maltim <timurm13@ukr.net>
2021-05-29 13:21:29 +0300

    Add project files.

---
+++ CheckersGame/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CheckersGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.pnlBack = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlBack.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Tomato;
+            this.btnStart.Location = new System.Drawing.Point(124, 105);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(171, 69);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.BackColor = System.Drawing.Color.Tomato;
+            this.btnRules.Location = new System.Drawing.Point(124, 180);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(171, 69);
+            this.btnRules.TabIndex = 1;
+            this.btnRules.Text = "Rules";
+            this.btnRules.UseVisualStyleBackColor = false;
+            this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
+            // 
+            // pnlBack
+            // 
+            this.pnlBack.BackColor = System.Drawing.Color.Coral;
+            this.pnlBack.Controls.Add(this.btnExit);
+            this.pnlBack.Controls.Add(this.btnStart);
+            this.pnlBack.Controls.Add(this.btnRules);
+            this.pnlBack.Location = new System.Drawing.Point(-2, -2);
+            this.pnlBack.Name = "pnlBack";
+            this.pnlBack.Size = new System.Drawing.Size(440, 852);
+            this.pnlBack.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Tomato;
+            this.btnExit.Location = new System.Drawing.Point(124, 255);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(171, 69);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 674);
+            this.Controls.Add(this.pnlBack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlBack.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Panel pnlBack;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ CheckersGame/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckersGame
+{
+    public partial class Form1 : Form
+    {
+        const int sizeOfMap = 8;
+        const int sizeOfCell = 50;
+        int currentPlayer;
+        Button previousMemorization;
+        bool isMoving;
+        int[,] map = new int[sizeOfMap, sizeOfMap];
+        Button[,] btn = new Button[sizeOfMap, sizeOfMap];
+        Image wchecker;
+        Image bchecker;
+        public Form1()
+        {
+            InitializeComponent();
+            wchecker = new Bitmap(new Bitmap(@"C:\Users\Комп\Desktop\Tvorchestvo\MyProjects(Kit)\CheckersGame\CheckersGame\bin\Debug\Materials\white.png"), new Size(sizeOfCell - 10, sizeOfCell - 10));
+            bchecker = new Bitmap(new Bitmap(@"C:\Users\Комп\Desktop\Tvorchestvo\MyProjects(Kit)\CheckersGame\CheckersGame\bin\Debug\Materials\black.png"), new Size(sizeOfCell - 10, sizeOfCell - 10));
+            this.Text = "Checkers";
+            Initialization();
+        }
+
+        public void Initialization()
+        {
+
+            currentPlayer = 1;
+            isMoving = false;
+            previousMemorization = null;
+            map = new int[sizeOfMap, sizeOfMap]{
+                {4,1,4,1,4,1,4,1 },
+                {1,4,1,4,1,4,1,4 },
+                {4,1,4,1,4,1,4,1 },
+                {3,4,3,4,3,4,3,4 },
+                {4,3,4,3,4,3,4,3 },
+                {2,4,2,4,2,4,2,4 },
+                {4,2,4,2,4,2,4,2 },
+                {2,4,2,4,2,4,2,4 },
+            };
+            MapCreation();
+        }
+
+        public void MapCreation()
+        {
+            this.Width = (sizeOfMap + 1) * sizeOfCell;
+            this.Height = (sizeOfMap + 1) * sizeOfCell;
+
+            for (int i = 0; i < sizeOfMap; i++)
+            {
+                for (int j = 0; j < sizeOfMap; j++)
+                {
+                    Button Cell = new Button();
+                    Cell.Location = new Point(j * sizeOfCell, i * sizeOfCell);
+                    Cell.Size = new Size(sizeOfCell, sizeOfCell);
+                    Cell.Click += new EventHandler(WhenCellPress);
+                    if (map[i, j] == 1)
+                    {
+                        Cell.Image = wchecker;
+                    }
+                    else if (map[i, j] == 2)
+                    {
+                        Cell.Image = bchecker;
+                    }
+                    Cell.BackColor = GetColour(Cell);
+                    this.Controls.Add(Cell);
+                }
+            }
+        }
+        public void SwitchPlayer()
+        {
+            currentPlayer = currentPlayer == 1 ? 2 : 1;
+        }
+
+        public Color GetColour(Button previousMemorization)
+        {
+            if ((previousMemorization.Location.Y / sizeOfCell % 2) != 0)
+            {
+                if ((previousMemorization.Location.X / sizeOfCell % 2) == 0)
+                {
+                    return Color.Black;
+                }
+            }
+            if ((previousMemorization.Location.Y / sizeOfCell % 2) == 0)
+            {
+                if ((previousMemorization.Location.X / sizeOfCell % 2) != 0)
+                {
+                    return Color.Black;
+                }
+            }
+            return Color.White;
+        }
+
+        public void WhenCellPress(object sender, EventArgs e)
+        {
+            if (previousMemorization != null)
+            {
+                previousMemorization.BackColor = GetColour(previousMemorization);
+            }
+            Button pressedButton = sender as Button;
+            if (map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] != 0 && map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] == currentPlayer)
+            {
+                pressedButton.BackColor = Color.Yellow;
+                isMoving = true;
+            }
+            else
+            {
+                if (isMoving)
+                {
+                    if(map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] != 2 
+                        && map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] != 1
+                        && map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] != 4)
+                    {
+
+                            pressedButton.Image = previousMemorization.Image;
+                            previousMemorization.Image = null;
+                            isMoving = false;
+                            SwitchPlayer();
+                         //int temp = map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell];
+                        ////map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] = map[previousMemorization.Location.Y / sizeOfCell, previousMemorization.Location.X / sizeOfCell];
+                        //map[pressedButton.Location.Y / sizeOfCell, pressedButton.Location.X / sizeOfCell] = temp;
+                    }
+                                   
+                }
+            }
+            previousMemorization = pressedButton;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Visible = false;
+            btnRules.Visible = false;
+            pnlBack.Visible = false;
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Доска 8х8, шашки занимают первые три ряда с каждой стороны; бить можно произвольное количество шашек в любых направлениях; простая ходит только вперед, но может бить назад, дамка может ходить на любое число полей; цель игры - съесть или запереть все шашки противника.");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
